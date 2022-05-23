1d4f682905465d47c21afa10b8742b60fd7b2871
fabianpg95 <fabian.portillo@wizeline.com>
2022-05-23 10:35:58 -0500

    Fix some errores in esLINT

--- src/frontend/cypress/integration/tests/login/Login.js
+++ src/frontend/cypress/integration/tests/login/Login.js
@@ -1,6 +1,24 @@
-import { Given, When, And, Then } from "cypress-cucumber-preprocessor/steps";
-import '../../pages/LoginPage'
+import { Given,When,Then,And } from "cypress-cucumber-preprocessor/steps";
+import LoginPage from '../../pages/LoginPage';
 
-Given('user is on login page', () => {
-    cy.visitLoginPage();
+Given('user is in login page', () => {
+    LoginPage.visitLoginPage();
 })
+
+When('user fill username {string}', (username) => {
+    LoginPage.fillUsername(username);
+})
+
+And('user fill password {string}', (password) => {
+    LoginPage.fillPassword(password);
+})
+
+And('clicks on log in button', () => {
+    LoginPage.clickOnLoginButton();
+})
+
+Then('user is redirected to MyNotes page', () => {
+    LoginPage.visitApp();
+})
+
+  
--- src/frontend/cypress/plugins/index.js
+++ src/frontend/cypress/plugins/index.js
@@ -1,9 +1,9 @@
-const reporter = require('cypress-mochawesome-reporter/plugin');
+//const reporter = require('cypress-mochawesome-reporter/plugin');
 const cucumber = require('cypress-cucumber-preprocessor').default
 const dotenvPlugin = require('cypress-dotenv');
 
 module.exports = (on, config) => {
-  rep = reporter(on);
+  //rep = reporter(on);
   on('file:preprocessor', cucumber())  
   config = dotenvPlugin(config)
 
